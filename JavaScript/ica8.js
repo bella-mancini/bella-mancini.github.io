b82032de18ee0a3c4f10cbf69ed0aa2bfff69e24
bella-mancini <89321746+bella-mancini@users.noreply.github.com>
2022-10-11 11:26:10 -0600

    midterm

---
+++ JavaScript/ica8.js
@@ -0,0 +1,13 @@
+let body = document.querySelector('body');
+//body.addEventListener('click', giveAlert);
+
+//function giveAlert() {
+ //   alert('this is an alert');
+//}
+
+var button = document.querySelector("your-button-id");
+button.addEventListener("click", function(e) {
+    // e is the event object
+    // e.target is the button element
+    // do stuff with them
+});
