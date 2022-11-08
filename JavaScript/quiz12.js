41a7ba37a02c546c4a6eb4fa0096bdae970b3789
bella-mancini <89321746+bella-mancini@users.noreply.github.com>
2022-11-08 10:39:14 -0700

    quiz10

---
+++ JavaScript/quiz12.js
@@ -0,0 +1,54 @@
+/* PLUS MINUS NUMBER ENTER*//*
+const output = document.querySelector('.output');
+let outputInt = parseInt(output.textContent);
+console.log(outputInt);
+
+const minusButton = document.querySelector('.minus-button').addEventListener('click', minus);
+const plusButton = document.querySelector('.plus-button').addEventListener('click', plus);
+const resetButton = document.querySelector('.reset-button').addEventListener('click', reset);
+const submitButton = document.querySelector('.submit-button').addEventListener('click', submit);
+const randomButton = document.querySelector('.random-button').addEventListener('click', random);
+
+
+
+function minus() {
+    if (outputInt > 0) {
+        outputInt -= 1;
+        output.textContent = outputInt;
+    }
+    
+}
+
+function submit() {
+    alert(output.textContent);
+}
+
+function reset() {
+    outputInt = 0000000000;
+    output.textContent = outputInt;
+}
+
+function plus() {
+    if (outputInt < 9999999999) {
+        outputInt += 1;
+        output.textContent = outputInt;
+    }
+}
+
+function random() {
+    outputInt = randomNumber(0, 999999999);
+    output.textContent = outputInt;
+}
+
+function randomNumber(min, max) {
+    const num = Math.floor(Math.random() * (max - min + 1)) + min;
+    return num;
+}*/
+
+/* SLIDER*/
+var slider = document.getEelmetnBYId("myRange");
+var sliderOutput = document.querySelector(".slider-output");
+
+function update() {
+    sliderOutput.textContent = slider.value;
+}
