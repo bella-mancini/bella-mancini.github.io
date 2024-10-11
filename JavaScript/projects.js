f52f005747b9687509f175a80983ff4d0e59abd0
bella-mancini <89321746+bella-mancini@users.noreply.github.com>
2024-10-11 11:34:10 -0600

    font size and clean up changes

--- JavaScript/projects.js
+++ JavaScript/projects.js
@@ -5,7 +5,7 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 
-/* For collapsing Box*/
+/* For Reusable drop down*/
 
 var coll = document.getElementsByClassName("collapsible");
 var i;
@@ -22,7 +22,7 @@ for (i = 0; i < coll.length; i++) {
   });
 }
 
-/* for collapsable side bar*/
+/* collapsable side bar*/
 var mini2 = false;
 
 
@@ -56,33 +56,7 @@ function closeNav() {
   document.getElementById("main").style.marginLeft = "85px";
 }
 
-
-/*let slideIndex = [1,1,1,1];
-
-let slideIndex = [1,1,1,1];
-
-let slideId = ["mySlides1", "mySlides2", "mySlides3", "mySlides4"]
-showSlides(1, 0);
-showSlides(1, 1);
-showSlides(1, 2);
-showSlides(1, 3);
-
-function plusSlides(n, no) {
-  showSlides(slideIndex[no] += n, no);
-}
-
-function showSlides(n, no) {
-  let i;
-  let x = document.getElementsByClassName(slideId[no]);
-  if (n > x.length) {slideIndex[no] = 1}    
-  if (n < 1) {slideIndex[no] = x.length}
-  for (i = 0; i < x.length; i++) {
-     x[i].style.display = "none";  
-  }
-  x[slideIndex[no]-1].style.display = "block";  
-}
-
-*/
+/* For slide deck */
 let slideIndex = [1, 1, 1, 1];
 let slideId = ["mySlides1", "mySlides2", "mySlides3", "mySlides4"];
 
