a774a4cb5c17b185ce20fb82bda0e6bd604658d3
bella-mancini <89321746+bella-mancini@users.noreply.github.com>
2024-12-10 11:48:38 -0700

    nav bar redesign

--- web-class/js/assign9.js
+++ web-class/js/assign9.js
@@ -6,13 +6,13 @@ const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
 
-const img = ['Photoshoot-1.jpg', `Photoshoot-2.jpg`, `Photoshoot-3.jpg`, `Photoshoot-4.jpg`, `Photoshoot-5.jpg`];
+const img = ['../../img/Photoshoot-1.jpg', `../../img/Photoshoot-2.jpg`, `../../img/Photoshoot-3.jpg`, `../../img/Photoshoot-4.jpg`, `../../img/Photoshoot-5.jpg`];
 const alts = {
-  'Photoshoot-1.jpg' : 'girl in purple',
-  'Photoshoot-2.jpg' : 'girl in purple',
-  'Photoshoot-3.jpg' : 'girl in red',
-  'Photoshoot-4.jpg' : 'girl in red',
-  'Photoshoot-5.jpg' : 'girl in blue'
+  '../../img/Photoshoot-1.jpg' : 'girl in purple',
+  '../../img/Photoshoot-2.jpg' : 'girl in purple',
+  '../../img/Photoshoot-3.jpg' : 'girl in red',
+  '../../img/Photoshoot-4.jpg' : 'girl in red',
+  '../../img/Photoshoot-5.jpg' : 'girl in blue'
 }
 
 /* Looping through images */
