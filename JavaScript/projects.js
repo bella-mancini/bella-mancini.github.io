6128f998864a68937a878b98eeb88c953b04f16d
bella-mancini <89321746+bella-mancini@users.noreply.github.com>
2024-12-16 12:25:42 -0700

    new nav

--- JavaScript/projects.js
+++ JavaScript/projects.js
@@ -22,6 +22,23 @@ for (i = 0; i < coll.length; i++) {
   });
 }
 
+/* For Reusable drop down*/
+
+var coll = document.getElementsByClassName("collapsible-nav");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.closest('div').nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
+
 /* collapsable side bar*/
 var mini2 = false;
 
