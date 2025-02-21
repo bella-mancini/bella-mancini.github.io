e410476376a672a2c97e28c22809ea27d9fbc8fc
bella-mancini <89321746+bella-mancini@users.noreply.github.com>
2025-02-21 15:32:04 -0700

    update filters and add 3d modeling back

--- JavaScript/projects.js
+++ JavaScript/projects.js
@@ -74,13 +74,15 @@ function closeNav() {
 }
 ///
 /* For slide deck */
-let slideIndex = [1, 1, 1, 1];
-let slideId = ["mySlides1", "mySlides2", "mySlides3", "mySlides4"];
+let slideIndex = [1, 1, 1, 1, 1, 1];
+let slideId = ["mySlides1", "mySlides2", "mySlides3", "mySlides4", "mySlides5", "mySlides6"];
 
 showSlides(1, 0);
 showSlides(1, 1);
 showSlides(1, 2);
 showSlides(1, 3);
+showSlides(1, 4);
+showSlides(1, 5);
 
 function plusSlides(n, no) {
   console.log(`Button clicked: Change slide in set ${no} by ${n}`);
